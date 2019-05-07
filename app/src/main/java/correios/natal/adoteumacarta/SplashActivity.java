127ff4e14b7c3c33a9cd178cb2b3cf85a07375b3
Gildo Neto <gildorama@gmail.com>
2019-05-07 19:30:57 -0300

    Inclusão do finish na Splash Activity

--- app/src/main/java/correios/natal/adoteumacarta/SplashActivity.java
+++ app/src/main/java/correios/natal/adoteumacarta/SplashActivity.java
@@ -20,6 +20,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
         };
 
